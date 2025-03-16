5c2abaea2944d17906291363cb87e30326bafad4
comma6760 <kinghojoon12@gmail.com>
2025-03-16 15:04:01 +0900

    modify code

--- 3.16/async/async_blocking.js
+++ 3.16/async/async_blocking.js
@@ -1,7 +1,9 @@
 // Async Blocking
 async function asyncBlockingTask() {
 	console.log("[Async Blocking] 작업 1 시작");
-	await new Promise((resolve) => setTimeout(resolve, 2000));
+	await new Promise((resolve) => {
+		setTimeout(resolve, 2000)
+	});
 // wating Async (2sec)
 	console.log("[Async Blocking] 작업 1 종료");
 }
